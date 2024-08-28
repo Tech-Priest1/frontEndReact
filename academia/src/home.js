407a861486b2361f785d9b67bc86390bab085609
Tech-Priest1 <petrus260@hotmail.com>
2024-08-28 19:27:46 -0300

    iniciando o projeto de react academia

---
+++ academia/src/home.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Login = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  const navigate = useNavigate()
+
+  const onButtonClick = () => {
+    
+    setEmailError('')
+    setPasswordError('')
+  
+   
+    if ('' === email) {
+      setEmailError('Digite um email')
+      return
+    }
+  
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      setEmailError('Digite um email valido')
+      return
+    }
+  
+    if ('' === password) {
+      setPasswordError('Digite uma senha')
+      return
+    }
+  
+    if (password.length < 5) {
+      setPasswordError('A senha precisa ter 5 ou mais caracteres')
+      return
+    }
+  
+    // Authentication calls will be made here...
+  }
+
+  return (
+    <div className={'mainContainer'}>
+      <div className={'titleContainer'}>
+        <div>Login</div>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          value={email}
+          placeholder="Digite seu email aqui"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          value={password}
+          placeholder="Digite sua senha aqui"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+      </div>
+    </div>
+  )
+}
+
+export default Login
