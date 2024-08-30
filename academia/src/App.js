20030099352d90ab13ce781bb89551c0dc297cd5
Tech-Priest1 <petrus260@hotmail.com>
2024-08-29 23:27:30 -0300

    editUserpage is working

--- academia/src/App.js
+++ academia/src/App.js
@@ -1,36 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './login/login'
-import Home from './home/home'
-import EditModality from './home/editModality'
-import Register from './login/register'
-import RegisterMember from './member/registerMember'
-import EditMember from './member/editMember'
-import EditUsers from './home/editUsers'
-import ProtectedRoute from './login/protectedRoute'
-import './App.css'
-import { useState, useEffect } from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './login/login';
+import Home from './home/home';
+import EditModality from './home/editModality';
+import Register from './login/register';
+import RegisterMember from './member/registerMember';
+import EditMember from './member/editMember';
+import EditUsers from './home/editUsers'; 
+import ProtectedRoute from './login/protectedRoute';
+import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
 
   useEffect(() => {
     const initialGymTypes = [
       {
         name: "Musculação",
         normalPrice: 50,
-        promotionalPrice: 40
+        promotionalPrice: 40,
       },
       {
-        name: "judô",
+        name: "Judô",
         normalPrice: 80,
-        promotionalPrice: 70
-      }
+        promotionalPrice: 70,
+      },
     ];
-  
+
     const storedGymTypes = JSON.parse(localStorage.getItem('gymTypes'));
-  
+
     if (!storedGymTypes || storedGymTypes.length === 0) {
       localStorage.setItem('gymTypes', JSON.stringify(initialGymTypes));
     }
@@ -48,32 +47,34 @@ function App() {
             path="/login"
             element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
           />
-         <Route
-         path="/register"
-         element={<Register />} 
-         />
-         <Route
-          path="/registerMember" 
-          element={<ProtectedRoute loggedIn={loggedIn}><RegisterMember /></ProtectedRoute>}
-         />
-         <Route 
-           path="/editMember/:id" 
-           element={<ProtectedRoute loggedIn={loggedIn}><EditMember /></ProtectedRoute>} />
-        
-           <Route 
-          path="/editModality" 
-          element={<ProtectedRoute loggedIn={loggedIn}><EditModality /></ProtectedRoute>} />
-          <Route 
-          path="/editUsers" 
-          element={<ProtectedRoute loggedIn={loggedIn}><editUsers /></ProtectedRoute>} />
-          <Route 
-            path="*" 
-            element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} 
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+          <Route
+            path="/registerMember"
+            element={<ProtectedRoute loggedIn={loggedIn}><RegisterMember /></ProtectedRoute>}
+          />
+          <Route
+            path="/editMember/:id"
+            element={<ProtectedRoute loggedIn={loggedIn}><EditMember /></ProtectedRoute>}
+          />
+          <Route
+            path="/editModality"
+            element={<ProtectedRoute loggedIn={loggedIn}><EditModality /></ProtectedRoute>}
+          />
+          <Route
+            path="/editUsers"
+            element={<ProtectedRoute loggedIn={loggedIn}><EditUsers /></ProtectedRoute>}
+          />
+          <Route
+            path="*"
+            element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
           />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- academia/src/home/editUsers.js
+++ academia/src/home/editUsers.js
@@ -1,110 +1,114 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const EditUsers = () => {
-  const [cpf, setCpf] = useState('')
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
- ;
-
+const EditModality = () => {
+  const [users, setUsers] = useState([]);
+  const [editingUserIndex, setEditingUserIndex] = useState(null);
+  const [editedName, setEditedName] = useState('');
+  const [editedEmail, setEditedEmail] = useState('');
+  const [editedPassword, setEditedPassword] = useState('');
+  const [editedCpf, setEditedCpf] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
-  
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    const usersToEdit = users.find(member => member.cpf === parseInt(cpf));
+    const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+    setUsers(storedUsers);
+  }, []);
 
-    if (usersToEdit) {
-      setName(usersToEdit.name);
-      setCpf(usersToEdit.cpf)
-      setEmail(usersToEdit.email);
-      setPassword(usersToEdit.password);
-      
-
-    } else {
-      alert('Membro não encontrado!');
-      navigate('/');
-    }
-  }, [cpf, navigate]);
-
-  
-
-  const onCancelClick = () => {
-    navigate('/');
+  const handleEditUser = (index) => {
+    const user = users[index];
+    setEditingUserIndex(index);
+    setEditedName(user.name);
+    setEditedEmail(user.email);
+    setEditedPassword(user.password);
+    setEditedCpf(user.cpf);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSaveUser = () => {
+    if (editingUserIndex !== null) {
+      const updatedUsers = [...users];
+      updatedUsers[editingUserIndex] = {
+        ...updatedUsers[editingUserIndex],
+        name: editedName,
+        email: editedEmail,
+        password: editedPassword,
+        cpf: editedCpf,
+      };
+      setUsers(updatedUsers);
+      localStorage.setItem('users', JSON.stringify(updatedUsers));
+      setEditingUserIndex(null);
+    }
+  };
 
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    const updatedusers = users.map(user =>
-      users.cpf === parseInt(cpf) ? { ...user, name, cpf, email, password } : users
-    );
+  const handleDeleteUser = (index) => {
+    const updatedUsers = users.filter((_, i) => i !== index);
+    setUsers(updatedUsers);
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+  };
 
-    localStorage.setItem('users', JSON.stringify(updatedusers));
-
+  const handleCancel = () => {
     navigate('/');
   };
 
   return (
-    <div className='mainContainer'>
-      <div className='titleContainer'>
-        <div>Registrar Novo Usuário</div>
+    <div className="userContainer">
+      <div>
+        <h3 className="userTitulo">Editar Usuários</h3>
       </div>
-      <br />
-      <form className='inputContainer' onSubmit={handleSubmit}>
-        <div>
+      <ul className="useryList">
+        {users.map((user, index) => (
+          <li key={index}> 
+            {user.name} - {user.email} - {user.cpf}
+            <div className="separador">
+            <button type="submit" className="inputButton" onClick={() => handleEditUser(index)}>Edit</button>
+            <button type="submit" className="inputButton" onClick={() => handleDeleteUser(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      {editingUserIndex !== null && (
+        <div className="fixedBottom">
           <input
             type="text"
-            placeholder="Nome"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className='inputBox'
-            required
+            placeholder="Name"
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+            className="inputBoxUser"
           />
-        </div>
-        <br />
-        <div>
           <input
             type="email"
             placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className='inputBox'
-            required
+            value={editedEmail}
+            onChange={(e) => setEditedEmail(e.target.value)}
+            className="inputBoxUser"
           />
-        </div>
-        <br />
-        <div>
           <input
             type="password"
-            placeholder="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className='inputBox'
-            required
+            placeholder="Password"
+            value={editedPassword}
+            onChange={(e) => setEditedPassword(e.target.value)}
+            className="inputBoxUser"
           />
-        </div>
-        <br />
-        <div>
           <input
             type="text"
             placeholder="CPF"
-            value={cpf}
-            onChange={(e) => setCpf(e.target.value)}
-            className='inputBox'
-            required
+            value={editedCpf}
+            onChange={(e) => setEditedCpf(e.target.value)}
+            className="inputBoxUser"
           />
+          <div className="separador">
+            <button  type="submit" className="inputButton" onClick={handleSaveUser}>Save</button>
+            <button type="submit" className="inputButton" onClick={() => setEditingUserIndex(null)}>Cancel</button>
+          </div>
         </div>
-        <br />
-        <div className="separador">
-          <button type="submit" className='inputButton'>Atualizar</button>
-          <button type="submit" className='inputButton' onClick={onCancelClick}>Cancelar</button>
-        </div>
-      </form>
+      )}
+
+      <div className="fixedBottom">
+        <button type="submit" className="inputButton" onClick={handleCancel}>Cancelar</button>
+      </div>
     </div>
   );
 };
 
-export default EditUsers;
+export default EditModality;
