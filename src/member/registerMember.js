50bf133f16445bc5cb243e738c7790111a9dccaf
Tech-Priest1 <petrus260@hotmail.com>
2024-10-02 09:53:26 -0300

    fixing input camp going behing the navbar

--- src/member/registerMember.js
+++ src/member/registerMember.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './registerMember.css';
 const RegisterMember = () => {
   const [name, setName] = useState('');
   const [cpf, setCpf] = useState('');
